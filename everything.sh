b746e1fd9d43c75e087d859335842ac56266e4e7
Vag Vagoff <vag.vagoff@gmail.com>
2015-08-15 12:32:46 +0300

    nim does not support dashes in module names, add workaround

--- everything.sh
+++ everything.sh
@@ -3,7 +3,7 @@ set -e
 ulimit -s 300999
 echo "[" > report.txt
 cat tests.txt | (cd critters; while read lng cmd; do
-lng="$lng" cmd="$cmd" bash -c "${cmd}"
+lng="$lng" cmd="$cmd" bash -c "${cmd}" >> ../compiler.log
 done)
 echo "]" >> report.txt
 cat report.txt
